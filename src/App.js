e38b99c879c98411d28b1185ae0dd610a2091efa
amir-shariati <seyed.amir.shariati@gmail.com>
2020-07-29 16:36:48 +0430

    counter is added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            count : 0
+        }
+
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this)
+    }
+
+    increment(){
+        console.log('Increment');
+    }
+
+    decrement(){
+        console.log('Decrement');
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Count: {this.state.count}</h2>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+            </div>
+        )
+  }
 }
 
-export default App;
+export default App
